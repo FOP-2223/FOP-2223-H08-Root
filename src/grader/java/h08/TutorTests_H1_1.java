6e56d5c654d24be7b400e965bca9b7a5242f28e0
Steyer, Nick <nick.steyer@stud.tu-darmstadt.de>
2022-09-27 00:36:20 +0200

    WIP: add test for H1.1

--- src/grader/java/h08/TutorTests_H1_1.java
+++ src/grader/java/h08/TutorTests_H1_1.java
@@ -1,5 +1,6 @@
 package h08;
 
+import h08.calculation.ArrayCalculatorWithPreconditions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.sourcegrade.jagr.api.rubric.TestForSubmission;
@@ -10,8 +11,19 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("H1.1")
 public class TutorTests_H1_1 {
     @Test
-    @DisplayName("1 | Existenz Klasse ")
+    @DisplayName("1 | Summenberechnung in ArrayCalculatorWithPreconditions ")
+    // TODO: parametrisierter Test
     public void t01() {
-        assertTrue(true);
+        var sut = new ArrayCalculatorWithPreconditions();
+
+        var array2 = new double[][]{
+            new double[]{1},
+            new double[]{1, 2},
+            new double[]{1, -2, 3},
+            new double[]{1, 2, 3, 4}
+        };
+
+        var actual = assertDoesNotThrow(() -> sut.addUp(array2, 50));
+        assertEquals(10, actual);
     }
 }
