739e20af2cba536094e656d14cd1b139080dd4e2
Steyer, Nick <nick.steyer@stud.tu-darmstadt.de>
2022-11-21 15:58:35 +0100

    Add index to MockPreconditions

--- src/graderPrivate/java/h08/MockPreconditions.java
+++ src/graderPrivate/java/h08/MockPreconditions.java
@@ -8,16 +8,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MockPreconditions {
-    public record CheckPrimaryArrayNotNullInvocation(double[][] primaryArray) {
+    public record CheckPrimaryArrayNotNullInvocation(double[][] primaryArray, int index) {
     }
 
-    public record CheckSecondaryArrayNotNullInvocation(double[][] primaryArray) {
+    public record CheckSecondaryArrayNotNullInvocation(double[][] primaryArray, int index) {
     }
 
-    public record CheckNumberNotNegativeInvocation(double number) {
+    public record CheckNumberNotNegativeInvocation(double number, int index) {
     }
 
-    public record CheckValuesInRangeInvocation(double[][] primaryArray, double max) {
+    public record CheckValuesInRangeInvocation(double[][] primaryArray, double max, int index) {
     }
 
     public static final List<CheckPrimaryArrayNotNullInvocation> CheckPrimaryArrayNotNullInvocations = new ArrayList<>();
@@ -25,6 +25,7 @@ public class MockPreconditions {
     public static final List<CheckNumberNotNegativeInvocation> CheckNumberNotNegativeInvocations = new ArrayList<>();
     public static final List<CheckValuesInRangeInvocation> CheckValuesInRangeInvocations = new ArrayList<>();
 
+    private static int index = 0;
     public static boolean forwardInvocations = true;
 
     public static void reset() {
@@ -33,31 +34,36 @@ public class MockPreconditions {
         CheckNumberNotNegativeInvocations.clear();
         CheckValuesInRangeInvocations.clear();
         forwardInvocations = true;
+        index = 0;
     }
 
     public static void checkPrimaryArrayNotNull(double[][] primaryArray) {
-        CheckPrimaryArrayNotNullInvocations.add(new CheckPrimaryArrayNotNullInvocation(primaryArray));
+        CheckPrimaryArrayNotNullInvocations.add(new CheckPrimaryArrayNotNullInvocation(primaryArray, index));
+        index++;
         if (forwardInvocations) {
             Preconditions.checkPrimaryArrayNotNull(primaryArray);
         }
     }
 
     public static void checkSecondaryArraysNotNull(double[][] primaryArray) {
-        CheckSecondaryArrayNotNullInvocations.add(new CheckSecondaryArrayNotNullInvocation(primaryArray));
+        CheckSecondaryArrayNotNullInvocations.add(new CheckSecondaryArrayNotNullInvocation(primaryArray, index));
+        index++;
         if (forwardInvocations) {
             Preconditions.checkSecondaryArraysNotNull(primaryArray);
         }
     }
 
     public static void checkNumberNotNegative(double number) throws WrongNumberException {
-        CheckNumberNotNegativeInvocations.add(new CheckNumberNotNegativeInvocation(number));
+        CheckNumberNotNegativeInvocations.add(new CheckNumberNotNegativeInvocation(number, index));
+        index++;
         if (forwardInvocations) {
             Preconditions.checkNumberNotNegative(number);
         }
     }
 
     public static void checkValuesInRange(double[][] primaryArray, double max) throws AtIndexPairException {
-        CheckValuesInRangeInvocations.add(new CheckValuesInRangeInvocation(primaryArray, max));
+        CheckValuesInRangeInvocations.add(new CheckValuesInRangeInvocation(primaryArray, max, index));
+        index++;
         if (forwardInvocations) {
             Preconditions.checkValuesInRange(primaryArray, max);
         }
